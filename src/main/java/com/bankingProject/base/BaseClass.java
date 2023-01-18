9b5eda9c395a085bbfb4e5815fad1c56b1760f08
Sayalii-kulkarni <sayalispkulkarni@gmail.com>
2023-01-18 21:06:43 +0530

    my first commit

---
+++ src/main/java/com/bankingProject/base/BaseClass.java
@@ -0,0 +1,17 @@
+package com.bankingProject.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.guru99.com/V1/index.php");
+		//driver.manage().window().maximize();
+	}
+
+
+
+}
